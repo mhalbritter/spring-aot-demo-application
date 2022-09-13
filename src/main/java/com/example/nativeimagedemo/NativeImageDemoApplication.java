46672c4c53726434f205cf9cd1f11c3d9fd0a26e
Moritz Halbritter <mkammerer@vmware.com>
2022-09-13 08:54:01 +0200

    Import code from start.spring.io

---
+++ src/main/java/com/example/nativeimagedemo/NativeImageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.nativeimagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeImageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeImageDemoApplication.class, args);
+	}
+
+}
